4258a39908ec624d41c8395d5424d760abcdef5d
dbrussee <dbrussee@gmail.com>
2017-07-23 20:20:02 -0400

    Tree callback and open/close methods

--- B/B2_0-Tree.js
+++ B/B2_0-Tree.js
@@ -1,20 +1,52 @@
 ﻿// B2.0 Tree
 // Added to bettway GIT
 
-B.Tree = function(elementId) {
+B.Tree = function(elementId, open_close_callback) {
+	if (open_close_callback == undefined) open_close_callback = function(branch) { };
+	this.callback = open_close_callback;
 	this.element = document.getElementById(elementId);
 	this.nodes = []; // A node can be a B.TreeLeaf or a B.TreeBranch (collection of B.TreeNodes)
 	return this;
 };
-B.Tree.prototype.addBranch = function(this, html, showing) {
-	var branch = new B.TreeBranch(html, showing);
+B.Tree.prototype.addBranch = function(html, showing) {
+	var branch = new B.TreeBranch(this, this, html, showing);
 	this.nodes.push(branch);
 	return branch;
 }
-B.Tree.prototype.addLeaf = function(this, txt, onclick) {
-	var leaf = new B.TreeLeaf(txt, onclick);
+B.Tree.prototype.addLeaf = function(txt, onclick) {
+	var leaf = new B.TreeLeaf(this, this, txt, onclick);
 	this.nodes.push(leaf);
 	return leaf;
+}
+B.Tree.prototype.closeAll = function() {
+	for (var i = 0; i < this.nodes.length; i++) {
+		var node = this.nodes[i]
+		if (node instanceof B.TreeBranch) {
+			node.closeAll();
+		}
+	}
+}
+B.Tree.prototype.closeAllBut = function(keep) {
+	var keepNode = keep;
+	if (typeof keep == "number") keepNode = this.nodes[keep];
+	for (var i = 0; i < this.nodes.length; i++) {
+		var node = this.nodes[i]
+		if (node instanceof B.TreeBranch) {
+			if (node == keep) {
+				if (!node.showing) node.open();
+			} else {
+				node.close();
+			}
+		}
+	}
+}
+B.Tree.prototype.openAll = function() {
+	for (var i = 0; i < this.nodes.length; i++) {
+		var node = this.nodes[i]
+		if (node instanceof B.TreeBranch) {
+			node.openAll();
+		}
+	}
 }
 B.Tree.prototype.render = function() {
 	for (var i = 0; i < this.nodes.length; i++) {
@@ -22,8 +54,9 @@ B.Tree.prototype.render = function() {
 	}
 }
 
-B.TreeBranch = function(tree, html, showing) {
+B.TreeBranch = function(tree, parent, html, showing) {
 	this.tree = tree;
+	this.parent = parent;
 	if (html == undefined) html = "Tree Branch";
 	this.html = html;
 	this.nodes = [];
@@ -32,14 +65,60 @@ B.TreeBranch = function(tree, html, showing) {
 	return this;
 }
 B.TreeBranch.prototype.addBranch = function(html, showing) {
-	var branch = new B.TreeBranch(this.tree, html, showing);
+	var branch = new B.TreeBranch(this.tree, this, html, showing);
 	this.nodes.push(branch);
 	return branch;
 }
 B.TreeBranch.prototype.addLeaf = function(html, onclick) {
-	var leaf = new B.TreeLeaf(this.tree, html, onclick);
+	var leaf = new B.TreeLeaf(this.tree, this, html, onclick);
 	this.nodes.push(leaf);
 	return leaf;
+}
+B.TreeBranch.prototype.close = function() {
+	$(this.ul).hide();
+	this.showing = false;
+	this.span.innerHTML = "&#x25B7; " + this.html;
+	this.tree.callback(this);
+	return this;
+}
+B.TreeBranch.prototype.closeAll = function() {
+	this.close();
+	for (var i = 0; i < this.nodes.length; i++) {
+		var node = this.nodes[i]
+		if (node instanceof B.TreeBranch) {
+			node.closeAll();
+		}
+	}
+}
+B.TreeBranch.prototype.closeAllBut = function(keep) {
+	var keepNode = keep;
+	if (typeof keep == "number") keepNode = this.nodes[keep];
+	for (var i = 0; i < this.nodes.length; i++) {
+		var node = this.nodes[i]
+		if (node instanceof B.TreeBranch) {
+			if (node == keep) {
+				if (!node.showing) node.open();
+			} else {
+				node.close();
+			}
+		}
+	}
+}
+B.TreeBranch.prototype.open = function() {
+	$(this.ul).show();
+	this.showing = true;
+	this.span.innerHTML = "&#x25BD; " + this.html;
+	this.tree.callback(this);
+	return this;
+}
+B.TreeBranch.prototype.openAll = function() {
+	this.open();
+	for (var i = 0; i < this.nodes.length; i++) {
+		var node = this.nodes[i]
+		if (node instanceof B.TreeBranch) {
+			node.openAll();
+		}
+	}
 }
 B.TreeBranch.prototype.render = function(parentElement) {
 	var li = document.createElement("li");
@@ -50,8 +129,10 @@ B.TreeBranch.prototype.render = function(parentElement) {
 	li.style.paddingLeft = "1.2em";
 	li.style.textIndent = "-1.2em";
 	var spn = document.createElement("span");
+	this.span = spn;
 	//spn.className = "branch";
 	spn.style.fontWeight = "bold";
+	spn.style.fontSize = "1.05em";
 	spn.style.cursor = "pointer";
 //	var chr = (this.showing ? "&#x25C7; " : "&#x25C6; ");
 	var chr = (this.showing ? "&#x25B7; " : "&#x25BD; ");
@@ -62,15 +143,9 @@ B.TreeBranch.prototype.render = function(parentElement) {
 		var branch = $(li).data("BBranch");
 		// branch is now this object
 		if (branch.showing) {
-			$(branch.ul).hide();
-			branch.showing = false;
-			spn.innerHTML = "&#x25B7; " + branch.html;
-//			B.addClass(branch.li, "showsub");
+			branch.close();
 		} else {
-			$(branch.ul).show();
-			branch.showing = true;
-			spn.innerHTML = "&#x25BD; " + branch.html;
-//			B.removeClass(branch.li, "showsub");
+			branch.open();
 		}
 	}
 	li.appendChild(spn);
@@ -89,8 +164,9 @@ B.TreeBranch.prototype.render = function(parentElement) {
 	parentElement.appendChild(li);
 }
 
-B.TreeLeaf = function(tree, html, onclick) {
+B.TreeLeaf = function(tree, parent, html, onclick) {
 	this.tree = tree;
+	this.parent = parent;
 	if (html == undefined) html = "";
 	this.html = html;
 	if (onclick == undefined || onclick=="") onclick = null;
